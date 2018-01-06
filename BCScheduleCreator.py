6a962c9afa19f9ca55872ed076f0f4a844e67bac
jdogmcsteezy <jesse.sasquatch.schmidt@gmail.com>
2018-01-06 15:21:54 -0800

    Wrap in function

--- BCScheduleCreator.py
+++ BCScheduleCreator.py
@@ -1,15 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
 
-driver = webdriver.Firefox()
-driver.get('http://searchclasses.butte.edu/')
-selection = Select(driver.find_element_by_id('InputTermId'))
-selection.select_by_visible_text('Spring 2018')
-selection = Select(driver.find_element_by_id('InputSubjectId'))
-selection.select_by_visible_text('CSCI - Computer Science')
-selection = Select(driver.find_element_by_id('InputLocationId'))
-selection.select_by_visible_text('Main Campus')
-driver.find_element_by_id('searchButton').click()
-print(driver.page_source)
-driver.quit()
+def GrabClassData(Term, Subject, Location):
+	driver = webdriver.Firefox()
+	driver.get('http://searchclasses.butte.edu/')
+	selection = Select(driver.find_element_by_id('InputTermId'))
+	selection.select_by_visible_text(Term)
+	selection = Select(driver.find_element_by_id('InputSubjectId'))
+	selection.select_by_visible_text(Subject)
+	selection = Select(driver.find_element_by_id('InputLocationId'))
+	selection.select_by_visible_text(Location)
+	driver.find_element_by_id('searchButton').click()
+	tableData = driver.page_source
+	driver.quit()
+	soup = BeautifulSoup(tableData)
+	
+	print(soup.prettify())
+
+GrabClassData('Spring 2018', 'CSCI - Computer Science', 'Main Campus')
