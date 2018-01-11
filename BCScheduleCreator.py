0491e0ec64f0c46068bd386d35dc21b313dcf70d
jdogmcsteezy <jesse.sasquatch.schmidt@gmail.com>
2018-01-10 19:24:58 -0800

    Parse html file into dict

--- BCScheduleCreator.py
+++ BCScheduleCreator.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 
+Subjects = ['AB - Agriculture Business, ACCT - Accounting, AET - Agricultural Engineering Tech, AGR - Agriculture, AGS - Agricultural Science, AJ - Administration of Justice, AJLE - AJ Law Enforcement, ALH - Allied Health, ANTH - Anthropology, ART - Art, ASL - American Sign Language, AUT - Automotive Technology, BCIS - Business Computer Information , BIOL - Biological Sciences, BUS - Business, CDF - Child Development &amp; Family Rel, CHEM - Chemistry, CHIN - Chinese, CLP - Career Life Planning, CMST - Communication Studies, COS - Cosmetology, CSCI - Computer Science, CSL - Counseling, DFT - Drafting, DRAM - Drama, DSPS - Disabled Student Programs/Serv, ECON - Economics, EDUC - Education, EH - Environmental Horticulture, EMS - Emergency Medical Services, ENGL - English, ENGR - Engineering, ESL - English as a Second Language, FASH - Fashion, FN - Foods &amp; Nutrition, FREN - French, FSC - Fire Science, GEOG - Geography, GEOL - Geology, GERM - German, HIM - Health Information Management, HIST - History, HLTH - Health, HON - Honors, HUM - Humanities, IDST - Interdisciplinary Studies, ITAL - Italian, JOUR - Journalism, JPN - Japanese, KIN - Kinesiology, LATN - Latin, LEAD - Language Education/Development, LIS - Library &amp; Information Science, LM - Life Management, MATH - Mathematics, MCS - Multicultural Studies, MSP - MultiMedia Studies Program, MUS - Music, NR - Natural Resources, NSG - Nursing, OLS - Occupational &amp; Life Skills, PE - Physical Education, PHIL - Philosophy, PHO - Photography, PHYS - Physics, POS - Political Science, PSC - Physical Science, PSY - Psychology, READ - Reading, RLS - Real Estate, RT - Respiratory Care, RTVF - Radio/TV/Film, SOC - Sociology, 'SPAN - Spanish, SPE - Special Education', 'WKE - Work Experience, WLD - Welding']
+
 def GrabClassData(Term, Location, Subject):
 	driver = webdriver.Firefox()
 	driver.get('http://searchclasses.butte.edu/')
@@ -23,4 +25,4 @@ def GrabClassData(Term, Location, Subject):
 	#print for testing
 	print(soup.prettify())
 
-GrabClassData('Spring 2018', 'Main Campus', None)
+GrabClassData('Spring 2018', 'Main Campus', '')
--- testBeautifulSoup.py
+++ testBeautifulSoup.py
@@ -1,70 +1,95 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
-import string
+import re
 
 def CleanText(string):
-	string = string.replace(r'\n', '')
-	string = string.replace('\'', '')
-	string = string.replace(',', '')
-	string = string.replace('*', '')
-	string = string.replace('  ', '')
-	return string.strip()
+    string = string.replace(r'\n', '')
+    string = string.replace('\'', '')
+    string = string.replace(',', '')
+    string = string.replace('*', '')
+    string = string.replace('  ', '')
+    return string.strip()
 
-def ParseSchedule(string):
-	schedule = {}
-	for type in string:
-		
-
-file = open('sampleHTML.html')
-
-htmlStr = [line for line in file]
-soup = BeautifulSoup(str(htmlStr), 'lxml')
+def ParseSchedule(classes):
+    schedule = {}
+    # Iterate through each type of class LEC or LAB
+    for classType in classes:
+        # To store Building, Days, Room, and start/stop time.
+        meeting = {}
+        # Find if it is LEC or LAB
+        pattern = re.compile(r'(LEC|LAB)')
+        match = pattern.search(classType)
+        # If LEC or LAB is not found then skip over class because it does not concern project.
+        if match is None:
+            continue
+        type_ = match.group()
+        # Find which building
+        pattern = re.compile(r'^\w{2,5}')
+        match = pattern.search(classType)
+        meeting['Building'] = match.group()
+        # Find which room
+        pattern = re.compile(r'\s\d{3}\s')
+        match = pattern.search(classType)
+        meeting['Room'] = match.group().strip()
+        # Find which days
+        pattern = re.compile(r'\s[MTWHhF]{1,6}\s')
+        match = pattern.search(classType)
+        # Check if the class even has official meeting times
+        if match is not None:
+            meeting['Days'] = match.group().strip()
+        else:
+            meeting['Days'] = None
+        # Find start and stop times
+        pattern = re.compile(r'\s\d{1,2}:\d{2}\s(AM|PM)')
+        # Save those time to a list
+        matches = [match for match in pattern.finditer(classType)]
+        times = ['Start','End']
+        for match,time in zip(matches, times):
+            #print(match,time)
+            meeting[time] = match.group().strip()
+        # Add the dic to a schedule
+        schedule[type_] = meeting
+    #return schedule dict.
+    return schedule
+    
+    
+with open('sampleHTML.html') as file:
+    soup = BeautifulSoup(file, 'lxml')
 rows = soup.find('tbody').findAll('tr')
-classInfo = {}
-classes = {}
-# for row in rows:
-# 	print('----------------------------------------')
-# 	for content in row.contents:
-# 		print(content)
-
-
-# --------This Finds Building, room, LEC/Lab, Day, Time
-# for row in rows:
-# 	for td in row.findAll('td', 'col-md-4'):
-# 		for div in td.findAll('div'):
-# 		# s = CleanText(td.find('div').text)
-
-# 			print(div.text)
-# 			print('\n')
-
+classes = []
 for row in rows:
-	classInfo = {}
-	LocationTime = []
-	for td in row.findAll('td', 'col-md-4'):
-		for div in td.findAll('div', limit=2):
-			text = div.text
-			if 'MC' in text:
-				LocationTime.append(CleanText(text))
-	if not LocationTime:
-		continue 
-	classInfo['LocationTime'] = LocationTime
-	for td in row.findAll('td', 'col-md-2'):
-		classInfo['Title'] = CleanText(td.text)
-	for td in row.findAll('a',href=True):
-		if 'http://www.butte.edu/district_info/directory' in td['href']:
-			classInfo['Instructor'] = CleanText(td.text)
+    classInfo = {}
+    LocationTime = []
+    for td in row.findAll('td', 'col-md-4'):
+        for div in td.findAll('div', limit=2):
+            text = div.text
+            if 'MC' in text:
+                LocationTime.append(CleanText(text))
+    if not LocationTime:
+        continue 
+    schedule = ParseSchedule(LocationTime)
+    if not schedule:
+        continue
+    classInfo['Schedule'] = schedule
+    td = row.find('td', 'col-md-2')
+    pattern = re.compile(r'[A-Z]\w{1,3}-\d+')
+    department_num = pattern.search(td.text).group()
+    pattern = re.compile(r'[A-Z]\d{4}\s.+')
+    name = pattern.search(td.text).group()
+    name = CleanText(name)[6:]
+    classInfo['Title'] = department_num + ' ' + name
+    for td in row.findAll('a',href=True):
+        if 'http://www.butte.edu/district_info/directory' in td['href']:
+            classInfo['Instructor'] = CleanText(td.text)
+    classes.append(classInfo)
+    print(classInfo)
+    print('\n')
 
-	print(classInfo)
-	print('\n')
-
-
-
-# for row in rows:
-# 	for td in row.findAll('a',href=True):
-# 		if 'http://www.butte.edu/district_info/directory' in td['href']:
-# 			s = CleanText(td.text)
-# 			print(s)
-# 			print('\n')
-		
+with open('subjectsHTML.txt') as f:
+    text = f.read()
+    pattern = re.compile(r'>.+<')
+    matches = pattern.finditer(text)
+    
+    with open ('subjects.txt','w') as w:
+        for match in matches:
+            w.write(match.group()[1:-1])
+            w.write('\', \'')
